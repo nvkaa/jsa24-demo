dc0a353758c70fcfd9ec1d3667b481a9ca9b230f
nvkaa <80764308+nvkaa@users.noreply.github.com>
2022-12-09 21:59:06 +0700

    Add files via upload

---
+++ signup.js
@@ -0,0 +1,37 @@
+$('#dropdownId').click(()=> {
+	$('.dropdown-menu').fadeToggle('slow');
+});
+
+
+
+const userArr = [];
+// const lsUserArr = JSON.stringify(userArr);
+localStorage.setItem('userArr', JSON.stringify(userArr));
+
+// console.log(userArr);
+var signup = 0;
+
+
+$('#submit').click(() => {
+     let usn = $('#usn').val();
+     let pw = $('#pw').val();
+
+     const user = {
+		Username: usn,
+		Password: pw,
+	}
+
+     if(user.Username !== '' && user.Password !== ''){
+		
+		userArr.push(user);
+		localStorage.setItem('userArr', JSON.stringify(userArr));
+		window.location='./login.html'
+		// console.log(signup);
+          // let login = 1;
+		// console.log(login);
+		
+	} else {
+		alert("Please fulfill your information")
+	}
+
+});
