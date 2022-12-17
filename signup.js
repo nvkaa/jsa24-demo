a4f0696a5bdba102ae579a4977d6a3d3360b5d26
nvkaa <80764308+nvkaa@users.noreply.github.com>
2022-12-17 21:31:26 +0700

    Update signup.js

--- signup.js
+++ signup.js
@@ -9,7 +9,7 @@ const userArr = [];
 localStorage.setItem('userArr', JSON.stringify(userArr));
 
 // console.log(userArr);
-var signup = 0;
+// var signup = 0;
 
 
 $('#submit').click(() => {
@@ -22,16 +22,34 @@ $('#submit').click(() => {
 	}
 
      if(user.Username !== '' && user.Password !== ''){
+		// if(){
+			userArr.push(user);
+			localStorage.setItem('userArr', JSON.stringify(userArr));
+			// window.location='./login.html'
+			console.log(userArr);
+			// let login = 1;
+			// console.log(login);
+		//}
 		
-		userArr.push(user);
-		localStorage.setItem('userArr', JSON.stringify(userArr));
-		window.location='./login.html'
-		// console.log(signup);
-          // let login = 1;
-		// console.log(login);
 		
 	} else {
 		alert("Please fulfill your information")
 	}
 
 });
+console.log(userArr);
+// $('#submitLogin').click(() => {
+// 	let usn = $('#usn').val();
+//      let pw = $('#pw').val();
+
+// 	for (let a = 0; a < userArr.length; a++){
+// 		if (userArr[a].Username == usn && userArr[a].Password == pw){
+// 			// window.location='./INdex.html'
+// 			alert('logged in')
+// 		} else {
+// 			alert("Username or password incorrecr, please try again.")
+// 		}
+		
+// 	}
+
+// });
