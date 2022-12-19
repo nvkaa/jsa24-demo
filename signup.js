2e1aa1aa7f78b2431576641242b69a46eab54784
nvkaa <80764308+nvkaa@users.noreply.github.com>
2022-12-19 14:26:44 +0700

    Update signup.js

--- signup.js
+++ signup.js
@@ -26,30 +26,37 @@ $('#submit').click(() => {
 			userArr.push(user);
 			localStorage.setItem('userArr', JSON.stringify(userArr));
 			// window.location='./login.html'
-			console.log(userArr);
+			// console.log(userArr);
 			// let login = 1;
 			// console.log(login);
+			$('.form').css({'display': 'none '})
+			//event.preventDefault($('.form2'))
+			$('.form2').css({'display': 'block '})
+
+			
 		//}
 		
 		
 	} else {
 		alert("Please fulfill your information")
+		window.location='./signup.html'
 	}
 
 });
 console.log(userArr);
-// $('#submitLogin').click(() => {
-// 	let usn = $('#usn').val();
-//      let pw = $('#pw').val();
 
-// 	for (let a = 0; a < userArr.length; a++){
-// 		if (userArr[a].Username == usn && userArr[a].Password == pw){
-// 			// window.location='./INdex.html'
-// 			alert('logged in')
-// 		} else {
-// 			alert("Username or password incorrecr, please try again.")
-// 		}
+$('#submitLogin').click(() => {
+	let usn = $('#usnL').val();
+     let pw = $('#pwL').val();
+
+	for (let a = 0; a < userArr.length; a++){
+		if (userArr[a].Username == usn && userArr[a].Password == pw){
+			// window.location='./INdex.html'
+			alert('logged in')
+		} else {
+			alert("Username or password incorrect , please try again.")
+		}
 		
-// 	}
+	}
 
-// });
+});
